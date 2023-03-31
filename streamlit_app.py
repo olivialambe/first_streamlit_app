1bc09c578c42a76df6f8fb7c7523453aad7820c2
olivialambe <129520549+olivialambe@users.noreply.github.com>
2023-03-31 14:23:22 +0100

    Update streamlit_app.py
    Adding smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text("🥣 Omego 3 and Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-range egg")
 streamlit.text("🥑🍞 Avocado Toast") 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
